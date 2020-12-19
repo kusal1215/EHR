810f7467a214ce542730a635040f385708da74ef
kusal1215 <kusalchathuranga2@gmail.com>
2020-12-19 12:03:52 +0530

    kusal: EHR login with auth and middleware

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends ParentAdminController
+{
+    public function index(){
+        return view('admin.admin');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('/ehr')->group(function () {
+    Route::get('/admin', 'AdminController@index')->name('AdminManager.admin');
+
+    Route::get('/doctor', 'DoctorController@index')->name('DoctorManager.doctor');
+
+    Route::get('/patient', 'PatientController@index')->name('PatientManager.patient');
+});
